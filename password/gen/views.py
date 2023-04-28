48b432eee47cbbaf41b0dbefcaac788cc1cf46f0
daniil vornikov <danilka_1971@mail.ru>
2023-04-28 14:06:19 +0300

    our first commit

---
+++ password/gen/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'gen/home.html')
+
+
+def password(request):
+
+
+
+    characters = list('abcdefghijklmopqrstuvxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIKLMOPQRSTUVXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#%^&*()_+'))
+    if request.GET.get('numbers'):
+        characters.extend(list('123456789'))
+
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'gen/password.html', {'password': thepassword })
+
+def abbaut(request):
+    return render(request, 'gen/abbaut.html')
